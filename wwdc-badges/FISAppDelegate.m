295cef07739ec3d19bfec99f084fbc3a7a77162e
bmanjuu <bettyofung@gmail.com>
2016-06-01 09:15:43 -0400

    second pass at badges lab

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -8,11 +8,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your solution here.
-     
-     */
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for(NSUInteger i=0; i < [conferenceSpeakers count]; i++){
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]);
+        // when NSLog-ing, can just put in format specifier, do not need to use the stringWithFormat method!
+    }
+    
     
     // do not alter
     return YES;  //
